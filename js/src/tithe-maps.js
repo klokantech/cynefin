0e6b90b3a699c129df751980afb6fcf915a82bb2
Petr Sloup <slouppetr@gmail.com>
2014-10-01 15:56:45 +0200

    Initial commit of the js compilation structure

---
+++ js/src/tithe-maps.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author petr.sloup@klokantech.com (Petr Sloup)
+ *
+ * Copyright 2014 Klokan Technologies Gmbh (www.klokantech.com)
+ */
+
+goog.provide('cynefin.TitheMaps');
+
+goog.require('ol.Map');
+
+
+
+/**
+ * @constructor
+ */
+cynefin.TitheMaps = function() {
+
+};
+
+
+goog.exportSymbol('TitheMaps', cynefin.TitheMaps);
