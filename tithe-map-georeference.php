8a5def85ccb0e62984d2f87f5b5ba6c9834739a5
Jakub Mikuláš <jakub@jedenbo>
2014-10-30 23:30:33 +0100

    Tithe maps - other layouts

---
+++ tithe-map-georeference.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Tithe maps georeference
+*/
+
+get_header();
+?>
+
+
+<section class="application">
+  <iframe id="app-frame" />
+</section>
+
+<script src="<?php bloginfo("template_url"); ?>/js/inittool.js"></script>
+<script>
+	initTool(2);
+</script>
+<style>
+	body, .page, .application {position:absolute;top:0;bottom:0;left:0;right:0;}
+	.application {top:120px;padding:0;}
+	#app-frame {width:100%;height:100%;border:none;}
+</style>
+
+
+<?php get_footer(); ?>
