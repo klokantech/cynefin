b496f7037e288fa97a689b217db97001dfca16f2
Dalibor Janák <dalibor.janak@gmail.com>
2014-11-24 09:17:28 +0100

    Video styling (klokan/gr4#101) moved to main.css

--- front-page.php
+++ front-page.php
@@ -133,11 +133,6 @@ new Home();
 </script>
 
 <!-- video -->
-<style>
-  #popup-bg{position: fixed; top: 0; right: 0; bottom: 0; left: 0; background-color: rgba(0, 0, 0, 0.6); z-index: 230; display: none;}
-  #popup-bg.active{display: block;}
-  #popup{position: absolute; width: 640px; height: 480px; top: 100px; left: 50%; margin-left: -330px; z-index: 250; padding: 15px; background-color: #F7F8FA; -webkit-border-radius: 6px; -moz-border-radius: 6px; border-radius: 6px;}
-</style>
 <div id="popup-bg">
   <div id="popup">
     <div id="popup-video"></div>
